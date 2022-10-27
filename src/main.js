c76318e76e0b23d38c849bd96365d5f0c67fdd10
Mathilde <mathilde@krogen5.dk>
2022-10-27 13:01:21 +0200

    final

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./style.css";
+import { animate, stagger, inView, scroll, timeline } from "motion";
+import Splide from '@splidejs/splide';
+new Splide( '#splide1' ).mount();
+new Splide( '#splide2' ).mount();
+new Splide( '#splide3' ).mount();
+new Splide( '#splide4' ).mount();
+import '@splidejs/splide/css';
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        header1: "'Aclonica', sans-serif",
+        font2: "'Julius Sans One', sans-serif",
+
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("daisyui")],
+};
