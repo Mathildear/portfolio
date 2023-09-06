9db9d25a11d53212c2351d108c80f079a118c941
Mathilde <mathilde@krogen5.dk>
2023-09-06 13:19:01 +0200

    Updates on projects

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion";
 import Splide from '@splidejs/splide';
+new Splide( '#splide0' ).mount();
 new Splide( '#splide1' ).mount();
 new Splide( '#splide2' ).mount();
 new Splide( '#splide3' ).mount();
